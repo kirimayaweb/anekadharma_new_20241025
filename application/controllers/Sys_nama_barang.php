9dd236d280934e7d7b9664c3f989ff979642cec9
“iwanesia.id” <iwanesia.id@gmail.com>
2024-12-04 20:21:45 +0700

    sys_nama_barang : refresh_data_from_persediaan_data ( ambil list data barang dari tabel persediaan )

--- application/controllers/Sys_nama_barang.php
+++ application/controllers/Sys_nama_barang.php
@@ -12,11 +12,38 @@ class Sys_nama_barang extends CI_Controller
         $this->load->library('form_validation');
     }
 
+    public function refresh_data_from_persediaan_data()
+    {
+        $sql = "SELECT `namabarang`,`satuan` FROM `persediaan` WHERE `namabarang`<>'' GROUP by `namabarang`";
+       
+        foreach ($this->db->query($sql)->result() as $m) {
+            // echo "<option value='$m->uuid_barang' ";
+            // echo ">  " . strtoupper($m->kode_barang) . strtoupper($m->namabarang)  . "</option>";
+       
+            $data = array(
+                // 'uuid_barang' => $this->input->post('uuid_barang',TRUE),
+                'kode_barang' => str_replace(" ","",$m->namabarang),
+                'nama_barang' => $m->namabarang,
+                'satuan' => $m->satuan,
+                // 'keterangan' => $this->input->post('keterangan', TRUE),
+            );
+
+            $this->Sys_nama_barang_model->insert($data);
+       
+       
+        }
+print_r("selesai");
+
+
+
+    }
+
+
     public function index()
     {
         $q = urldecode($this->input->get('q', TRUE));
         $start = intval($this->input->get('start'));
-        
+
         if ($q <> '') {
             $config['base_url'] = base_url() . 'sys_nama_barang/index.html?q=' . urlencode($q);
             $config['first_url'] = base_url() . 'sys_nama_barang/index.html?q=' . urlencode($q);
@@ -44,18 +71,18 @@ class Sys_nama_barang extends CI_Controller
         $this->template->load('anekadharma/adminlte310_anekadharma_topnav_aside', 'anekadharma/sys_nama_barang/sys_nama_barang_list', $data);
     }
 
-    public function read($id) 
+    public function read($id)
     {
         $row = $this->Sys_nama_barang_model->get_by_id($id);
         if ($row) {
             $data = array(
-		'id' => $row->id,
-		'uuid_barang' => $row->uuid_barang,
-		'kode_barang' => $row->kode_barang,
-		'nama_barang' => $row->nama_barang,
-		'satuan' => $row->satuan,
-		'keterangan' => $row->keterangan,
-	    );
+                'id' => $row->id,
+                'uuid_barang' => $row->uuid_barang,
+                'kode_barang' => $row->kode_barang,
+                'nama_barang' => $row->nama_barang,
+                'satuan' => $row->satuan,
+                'keterangan' => $row->keterangan,
+            );
             $this->load->view('sys_nama_barang/sys_nama_barang_read', $data);
         } else {
             $this->session->set_flashdata('message', 'Record Not Found');
@@ -63,23 +90,23 @@ class Sys_nama_barang extends CI_Controller
         }
     }
 
-    public function create() 
+    public function create()
     {
         $data = array(
             'button' => 'Create',
             'action' => site_url('sys_nama_barang/create_action'),
-	    'id' => set_value('id'),
-	    'uuid_barang' => set_value('uuid_barang'),
-	    'kode_barang' => set_value('kode_barang'),
-	    'nama_barang' => set_value('nama_barang'),
-	    'satuan' => set_value('satuan'),
-	    'keterangan' => set_value('keterangan'),
-	);
+            'id' => set_value('id'),
+            'uuid_barang' => set_value('uuid_barang'),
+            'kode_barang' => set_value('kode_barang'),
+            'nama_barang' => set_value('nama_barang'),
+            'satuan' => set_value('satuan'),
+            'keterangan' => set_value('keterangan'),
+        );
         // $this->load->view('sys_nama_barang/sys_nama_barang_form', $data);
         $this->template->load('anekadharma/adminlte310_anekadharma_topnav_aside', 'anekadharma/sys_nama_barang/sys_nama_barang_form', $data);
     }
-    
-    public function create_action() 
+
+    public function create_action()
     {
         $this->_rules();
 
@@ -87,20 +114,20 @@ class Sys_nama_barang extends CI_Controller
             $this->create();
         } else {
             $data = array(
-		// 'uuid_barang' => $this->input->post('uuid_barang',TRUE),
-		'kode_barang' => $this->input->post('kode_barang',TRUE),
-		'nama_barang' => $this->input->post('nama_barang',TRUE),
-		'satuan' => $this->input->post('satuan',TRUE),
-		'keterangan' => $this->input->post('keterangan',TRUE),
-	    );
+                // 'uuid_barang' => $this->input->post('uuid_barang',TRUE),
+                'kode_barang' => $this->input->post('kode_barang', TRUE),
+                'nama_barang' => $this->input->post('nama_barang', TRUE),
+                'satuan' => $this->input->post('satuan', TRUE),
+                'keterangan' => $this->input->post('keterangan', TRUE),
+            );
 
             $this->Sys_nama_barang_model->insert($data);
             $this->session->set_flashdata('message', 'Create Record Success');
             redirect(site_url('sys_nama_barang'));
         }
     }
-    
-    public function update($id) 
+
+    public function update($id)
     {
         $row = $this->Sys_nama_barang_model->get_by_id($id);
 
@@ -108,21 +135,21 @@ class Sys_nama_barang extends CI_Controller
             $data = array(
                 'button' => 'Update',
                 'action' => site_url('sys_nama_barang/update_action'),
-		'id' => set_value('id', $row->id),
-		// 'uuid_barang' => set_value('uuid_barang', $row->uuid_barang),
-		'kode_barang' => set_value('kode_barang', $row->kode_barang),
-		'nama_barang' => set_value('nama_barang', $row->nama_barang),
-		'satuan' => set_value('satuan', $row->satuan),
-		'keterangan' => set_value('keterangan', $row->keterangan),
-	    );
+                'id' => set_value('id', $row->id),
+                // 'uuid_barang' => set_value('uuid_barang', $row->uuid_barang),
+                'kode_barang' => set_value('kode_barang', $row->kode_barang),
+                'nama_barang' => set_value('nama_barang', $row->nama_barang),
+                'satuan' => set_value('satuan', $row->satuan),
+                'keterangan' => set_value('keterangan', $row->keterangan),
+            );
             $this->load->view('sys_nama_barang/sys_nama_barang_form', $data);
         } else {
             $this->session->set_flashdata('message', 'Record Not Found');
             redirect(site_url('sys_nama_barang'));
         }
     }
-    
-    public function update_action() 
+
+    public function update_action()
     {
         $this->_rules();
 
@@ -130,20 +157,20 @@ class Sys_nama_barang extends CI_Controller
             $this->update($this->input->post('id', TRUE));
         } else {
             $data = array(
-		// 'uuid_barang' => $this->input->post('uuid_barang',TRUE),
-		'kode_barang' => $this->input->post('kode_barang',TRUE),
-		'nama_barang' => $this->input->post('nama_barang',TRUE),
-		'satuan' => $this->input->post('satuan',TRUE),
-		'keterangan' => $this->input->post('keterangan',TRUE),
-	    );
+                // 'uuid_barang' => $this->input->post('uuid_barang',TRUE),
+                'kode_barang' => $this->input->post('kode_barang', TRUE),
+                'nama_barang' => $this->input->post('nama_barang', TRUE),
+                'satuan' => $this->input->post('satuan', TRUE),
+                'keterangan' => $this->input->post('keterangan', TRUE),
+            );
 
             $this->Sys_nama_barang_model->update($this->input->post('id', TRUE), $data);
             $this->session->set_flashdata('message', 'Update Record Success');
             redirect(site_url('sys_nama_barang'));
         }
     }
-    
-    public function delete($id) 
+
+    public function delete($id)
     {
         $row = $this->Sys_nama_barang_model->get_by_id($id);
 
@@ -157,16 +184,16 @@ class Sys_nama_barang extends CI_Controller
         }
     }
 
-    public function _rules() 
+    public function _rules()
     {
-	// $this->form_validation->set_rules('uuid_barang', 'uuid barang', 'trim|required');
-	// $this->form_validation->set_rules('kode_barang', 'kode barang', 'trim|required');
-	$this->form_validation->set_rules('nama_barang', 'nama barang', 'trim|required');
-	// $this->form_validation->set_rules('satuan', 'satuan', 'trim|required');
-	// $this->form_validation->set_rules('keterangan', 'keterangan', 'trim|required');
+        // $this->form_validation->set_rules('uuid_barang', 'uuid barang', 'trim|required');
+        // $this->form_validation->set_rules('kode_barang', 'kode barang', 'trim|required');
+        $this->form_validation->set_rules('nama_barang', 'nama barang', 'trim|required');
+        // $this->form_validation->set_rules('satuan', 'satuan', 'trim|required');
+        // $this->form_validation->set_rules('keterangan', 'keterangan', 'trim|required');
 
-	$this->form_validation->set_rules('id', 'id', 'trim');
-	$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+        $this->form_validation->set_rules('id', 'id', 'trim');
+        $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
     }
 
     public function excel()
@@ -191,31 +218,30 @@ class Sys_nama_barang extends CI_Controller
 
         $kolomhead = 0;
         xlsWriteLabel($tablehead, $kolomhead++, "No");
-	xlsWriteLabel($tablehead, $kolomhead++, "Uuid Barang");
-	xlsWriteLabel($tablehead, $kolomhead++, "Kode Barang");
-	xlsWriteLabel($tablehead, $kolomhead++, "Nama Barang");
-	xlsWriteLabel($tablehead, $kolomhead++, "Satuan");
-	xlsWriteLabel($tablehead, $kolomhead++, "Keterangan");
+        xlsWriteLabel($tablehead, $kolomhead++, "Uuid Barang");
+        xlsWriteLabel($tablehead, $kolomhead++, "Kode Barang");
+        xlsWriteLabel($tablehead, $kolomhead++, "Nama Barang");
+        xlsWriteLabel($tablehead, $kolomhead++, "Satuan");
+        xlsWriteLabel($tablehead, $kolomhead++, "Keterangan");
 
-	foreach ($this->Sys_nama_barang_model->get_all() as $data) {
+        foreach ($this->Sys_nama_barang_model->get_all() as $data) {
             $kolombody = 0;
 
             //ubah xlsWriteLabel menjadi xlsWriteNumber untuk kolom numeric
             xlsWriteNumber($tablebody, $kolombody++, $nourut);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->uuid_barang);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->kode_barang);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->nama_barang);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->satuan);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->keterangan);
+            xlsWriteLabel($tablebody, $kolombody++, $data->uuid_barang);
+            xlsWriteLabel($tablebody, $kolombody++, $data->kode_barang);
+            xlsWriteLabel($tablebody, $kolombody++, $data->nama_barang);
+            xlsWriteLabel($tablebody, $kolombody++, $data->satuan);
+            xlsWriteLabel($tablebody, $kolombody++, $data->keterangan);
 
-	    $tablebody++;
+            $tablebody++;
             $nourut++;
         }
 
         xlsEOF();
         exit();
     }
-
 }
 
 /* End of file Sys_nama_barang.php */
