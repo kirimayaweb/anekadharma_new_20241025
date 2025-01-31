5e05dd2f83b94c04c24983ca78eadf49ed7614b8
“iwanesia.id” <iwanesia.id@gmail.com>
2025-01-31 07:26:05 +0700

    pembayaran ke supplier : kontrol L dan kas

--- application/views/anekadharma/pembayaran/adminlte310_pembayaran_list_ke_supplier.php
+++ application/views/anekadharma/pembayaran/adminlte310_pembayaran_list_ke_supplier.php
@@ -106,23 +106,39 @@
 
                                             } elseif ($list_data->statuslu == "L" and $list_data->kas_bank == "kas") {
                                                 // echo nominal($list_data->total_pembelian);
-                                                echo number_format($list_data->nominal_pengajuan, 2, ',', '.');
-                                                $Total_pembayaran = $Total_pembayaran + $list_data->nominal_pengajuan;
+                                                if($list_data->nominal_pengajuan > 0){
+                                                    echo number_format($list_data->nominal_pengajuan, 2, ',', '.');
+                                                    $Total_pembayaran = $Total_pembayaran + $list_data->nominal_pengajuan;
+                                                }else{
+                                                    echo number_format($list_data->total_pembelian, 2, ',', '.');
+                                                    $Total_pembayaran = $Total_pembayaran + $list_data->total_pembelian;
+                                                }
+
 
                                             } elseif ($list_data->statuslu == "L" and $list_data->kas_bank == "bank") {
                                                 // echo nominal($list_data->total_pembelian);
-                                                echo number_format($list_data->total_pembelian, 2, ',', '.');
-                                                $Total_pembayaran = $Total_pembayaran + $list_data->total_pembelian;
-                                            
+                                                if($list_data->nominal_pengajuan > 0){
+                                                    echo number_format($list_data->nominal_pengajuan, 2, ',', '.');
+                                                    $Total_pembayaran = $Total_pembayaran + $list_data->nominal_pengajuan;
+                                                }else{
+                                                    echo number_format($list_data->total_pembelian, 2, ',', '.');
+                                                    $Total_pembayaran = $Total_pembayaran + $list_data->total_pembelian;
+                                                }
                                             } elseif ($list_data->statuslu == "L") {
                                                 // echo nominal($list_data->total_pembelian);
-                                                echo number_format($list_data->total_pembelian, 2, ',', '.');
-                                                $Total_pembayaran = $Total_pembayaran + $list_data->total_pembelian;
+                                                if($list_data->nominal_pengajuan > 0){
+                                                    echo number_format($list_data->nominal_pengajuan, 2, ',', '.');
+                                                    $Total_pembayaran = $Total_pembayaran + $list_data->nominal_pengajuan;
+                                                }else{
+                                                    echo number_format($list_data->total_pembelian, 2, ',', '.');
+                                                    $Total_pembayaran = $Total_pembayaran + $list_data->total_pembelian;
+                                                }
                                             }
 
                                             // NOTE:
                                             //  L KAS ==> DARI TABEL KAS KECIL
                                             //  L BANK ?  ==> DARI FORM MANA ?
+                                            // L ??? tanpa kas/bank
                                             ?>
                                         </td>
 
