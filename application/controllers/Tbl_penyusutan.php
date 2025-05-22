0661b1c631a63cb8ad91ce55f27071119960ba31
“iwanesia.id” <iwanesia.id@gmail.com>
2025-05-22 12:42:42 +0700

    KODE - KODE PENYUSUTAN

--- application/controllers/Tbl_penyusutan.php
+++ application/controllers/Tbl_penyusutan.php
@@ -9,51 +9,56 @@ class Tbl_penyusutan extends CI_Controller
     {
         parent::__construct();
         $this->load->model('Tbl_penyusutan_model');
-        $this->load->library('form_validation');        
-	$this->load->library('datatables');
+        $this->load->library('form_validation');
+        $this->load->library('datatables');
     }
+
+    // KODE - KODE YANG MASUK KE DATA PENYUSUTAN
+    // Itu persediaan pak, kalo penyusutan 13101, 13102, 13103, 13104, 13105, 13201
+
 
     public function index_server_side()
     {
         $this->load->view('anekadharma/tbl_penyusutan/tbl_penyusutan_list');
     }
 
+
+
+
     public function index()
     {
 
         $data = array(
-			'Data_penyusutan' => "",
-			// 'action' => site_url('Buku_besar/cari_kode_akun'),
-		);
-		
-        $this->template->load('anekadharma/adminlte310_anekadharma_topnav_aside', 'anekadharma/tbl_penyusutan/adminlte310_tbl_penyusutan_list', $data);
+            'Data_penyusutan' => "",
+            // 'action' => site_url('Buku_besar/cari_kode_akun'),
+        );
 
-        
-    
-    } 
-    
-    public function json() {
+        $this->template->load('anekadharma/adminlte310_anekadharma_topnav_aside', 'anekadharma/tbl_penyusutan/adminlte310_tbl_penyusutan_list', $data);
+    }
+
+    public function json()
+    {
         header('Content-Type: application/json');
         echo $this->Tbl_penyusutan_model->json();
     }
 
-    public function read($id) 
+    public function read($id)
     {
         $row = $this->Tbl_penyusutan_model->get_by_id($id);
         if ($row) {
             $data = array(
-		'id' => $row->id,
-		'uuid_penyusutan' => $row->uuid_penyusutan,
-		'kelompok_harta' => $row->kelompok_harta,
-		'tanggal_perolehan' => $row->tanggal_perolehan,
-		'harga_perolehan' => $row->harga_perolehan,
-		'user' => $row->user,
-		'armorst_penyusutan_thn_lalu' => $row->armorst_penyusutan_thn_lalu,
-		'nilai_buku_thn_lalu' => $row->nilai_buku_thn_lalu,
-		'penyusutan_bulan_ini' => $row->penyusutan_bulan_ini,
-		'armorst_penyusutan_bulan_ini' => $row->armorst_penyusutan_bulan_ini,
-		'nilai_buku_bulan_ini' => $row->nilai_buku_bulan_ini,
-	    );
+                'id' => $row->id,
+                'uuid_penyusutan' => $row->uuid_penyusutan,
+                'kelompok_harta' => $row->kelompok_harta,
+                'tanggal_perolehan' => $row->tanggal_perolehan,
+                'harga_perolehan' => $row->harga_perolehan,
+                'user' => $row->user,
+                'armorst_penyusutan_thn_lalu' => $row->armorst_penyusutan_thn_lalu,
+                'nilai_buku_thn_lalu' => $row->nilai_buku_thn_lalu,
+                'penyusutan_bulan_ini' => $row->penyusutan_bulan_ini,
+                'armorst_penyusutan_bulan_ini' => $row->armorst_penyusutan_bulan_ini,
+                'nilai_buku_bulan_ini' => $row->nilai_buku_bulan_ini,
+            );
             $this->load->view('tbl_penyusutan/tbl_penyusutan_read', $data);
         } else {
             $this->session->set_flashdata('message', 'Record Not Found');
@@ -61,27 +66,27 @@ class Tbl_penyusutan extends CI_Controller
         }
     }
 
-    public function create() 
+    public function create()
     {
         $data = array(
             'button' => 'Create',
             'action' => site_url('tbl_penyusutan/create_action'),
-	    'id' => set_value('id'),
-	    'uuid_penyusutan' => set_value('uuid_penyusutan'),
-	    'kelompok_harta' => set_value('kelompok_harta'),
-	    'tanggal_perolehan' => set_value('tanggal_perolehan'),
-	    'harga_perolehan' => set_value('harga_perolehan'),
-	    'user' => set_value('user'),
-	    'armorst_penyusutan_thn_lalu' => set_value('armorst_penyusutan_thn_lalu'),
-	    'nilai_buku_thn_lalu' => set_value('nilai_buku_thn_lalu'),
-	    'penyusutan_bulan_ini' => set_value('penyusutan_bulan_ini'),
-	    'armorst_penyusutan_bulan_ini' => set_value('armorst_penyusutan_bulan_ini'),
-	    'nilai_buku_bulan_ini' => set_value('nilai_buku_bulan_ini'),
-	);
+            'id' => set_value('id'),
+            'uuid_penyusutan' => set_value('uuid_penyusutan'),
+            'kelompok_harta' => set_value('kelompok_harta'),
+            'tanggal_perolehan' => set_value('tanggal_perolehan'),
+            'harga_perolehan' => set_value('harga_perolehan'),
+            'user' => set_value('user'),
+            'armorst_penyusutan_thn_lalu' => set_value('armorst_penyusutan_thn_lalu'),
+            'nilai_buku_thn_lalu' => set_value('nilai_buku_thn_lalu'),
+            'penyusutan_bulan_ini' => set_value('penyusutan_bulan_ini'),
+            'armorst_penyusutan_bulan_ini' => set_value('armorst_penyusutan_bulan_ini'),
+            'nilai_buku_bulan_ini' => set_value('nilai_buku_bulan_ini'),
+        );
         $this->load->view('tbl_penyusutan/tbl_penyusutan_form', $data);
     }
-    
-    public function create_action() 
+
+    public function create_action()
     {
         $this->_rules();
 
@@ -89,25 +94,25 @@ class Tbl_penyusutan extends CI_Controller
             $this->create();
         } else {
             $data = array(
-		'uuid_penyusutan' => $this->input->post('uuid_penyusutan',TRUE),
-		'kelompok_harta' => $this->input->post('kelompok_harta',TRUE),
-		'tanggal_perolehan' => $this->input->post('tanggal_perolehan',TRUE),
-		'harga_perolehan' => $this->input->post('harga_perolehan',TRUE),
-		'user' => $this->input->post('user',TRUE),
-		'armorst_penyusutan_thn_lalu' => $this->input->post('armorst_penyusutan_thn_lalu',TRUE),
-		'nilai_buku_thn_lalu' => $this->input->post('nilai_buku_thn_lalu',TRUE),
-		'penyusutan_bulan_ini' => $this->input->post('penyusutan_bulan_ini',TRUE),
-		'armorst_penyusutan_bulan_ini' => $this->input->post('armorst_penyusutan_bulan_ini',TRUE),
-		'nilai_buku_bulan_ini' => $this->input->post('nilai_buku_bulan_ini',TRUE),
-	    );
+                'uuid_penyusutan' => $this->input->post('uuid_penyusutan', TRUE),
+                'kelompok_harta' => $this->input->post('kelompok_harta', TRUE),
+                'tanggal_perolehan' => $this->input->post('tanggal_perolehan', TRUE),
+                'harga_perolehan' => $this->input->post('harga_perolehan', TRUE),
+                'user' => $this->input->post('user', TRUE),
+                'armorst_penyusutan_thn_lalu' => $this->input->post('armorst_penyusutan_thn_lalu', TRUE),
+                'nilai_buku_thn_lalu' => $this->input->post('nilai_buku_thn_lalu', TRUE),
+                'penyusutan_bulan_ini' => $this->input->post('penyusutan_bulan_ini', TRUE),
+                'armorst_penyusutan_bulan_ini' => $this->input->post('armorst_penyusutan_bulan_ini', TRUE),
+                'nilai_buku_bulan_ini' => $this->input->post('nilai_buku_bulan_ini', TRUE),
+            );
 
             $this->Tbl_penyusutan_model->insert($data);
             $this->session->set_flashdata('message', 'Create Record Success');
             redirect(site_url('tbl_penyusutan'));
         }
     }
-    
-    public function update($id) 
+
+    public function update($id)
     {
         $row = $this->Tbl_penyusutan_model->get_by_id($id);
 
@@ -115,26 +120,26 @@ class Tbl_penyusutan extends CI_Controller
             $data = array(
                 'button' => 'Update',
                 'action' => site_url('tbl_penyusutan/update_action'),
-		'id' => set_value('id', $row->id),
-		'uuid_penyusutan' => set_value('uuid_penyusutan', $row->uuid_penyusutan),
-		'kelompok_harta' => set_value('kelompok_harta', $row->kelompok_harta),
-		'tanggal_perolehan' => set_value('tanggal_perolehan', $row->tanggal_perolehan),
-		'harga_perolehan' => set_value('harga_perolehan', $row->harga_perolehan),
-		'user' => set_value('user', $row->user),
-		'armorst_penyusutan_thn_lalu' => set_value('armorst_penyusutan_thn_lalu', $row->armorst_penyusutan_thn_lalu),
-		'nilai_buku_thn_lalu' => set_value('nilai_buku_thn_lalu', $row->nilai_buku_thn_lalu),
-		'penyusutan_bulan_ini' => set_value('penyusutan_bulan_ini', $row->penyusutan_bulan_ini),
-		'armorst_penyusutan_bulan_ini' => set_value('armorst_penyusutan_bulan_ini', $row->armorst_penyusutan_bulan_ini),
-		'nilai_buku_bulan_ini' => set_value('nilai_buku_bulan_ini', $row->nilai_buku_bulan_ini),
-	    );
+                'id' => set_value('id', $row->id),
+                'uuid_penyusutan' => set_value('uuid_penyusutan', $row->uuid_penyusutan),
+                'kelompok_harta' => set_value('kelompok_harta', $row->kelompok_harta),
+                'tanggal_perolehan' => set_value('tanggal_perolehan', $row->tanggal_perolehan),
+                'harga_perolehan' => set_value('harga_perolehan', $row->harga_perolehan),
+                'user' => set_value('user', $row->user),
+                'armorst_penyusutan_thn_lalu' => set_value('armorst_penyusutan_thn_lalu', $row->armorst_penyusutan_thn_lalu),
+                'nilai_buku_thn_lalu' => set_value('nilai_buku_thn_lalu', $row->nilai_buku_thn_lalu),
+                'penyusutan_bulan_ini' => set_value('penyusutan_bulan_ini', $row->penyusutan_bulan_ini),
+                'armorst_penyusutan_bulan_ini' => set_value('armorst_penyusutan_bulan_ini', $row->armorst_penyusutan_bulan_ini),
+                'nilai_buku_bulan_ini' => set_value('nilai_buku_bulan_ini', $row->nilai_buku_bulan_ini),
+            );
             $this->load->view('tbl_penyusutan/tbl_penyusutan_form', $data);
         } else {
             $this->session->set_flashdata('message', 'Record Not Found');
             redirect(site_url('tbl_penyusutan'));
         }
     }
-    
-    public function update_action() 
+
+    public function update_action()
     {
         $this->_rules();
 
@@ -142,25 +147,25 @@ class Tbl_penyusutan extends CI_Controller
             $this->update($this->input->post('id', TRUE));
         } else {
             $data = array(
-		'uuid_penyusutan' => $this->input->post('uuid_penyusutan',TRUE),
-		'kelompok_harta' => $this->input->post('kelompok_harta',TRUE),
-		'tanggal_perolehan' => $this->input->post('tanggal_perolehan',TRUE),
-		'harga_perolehan' => $this->input->post('harga_perolehan',TRUE),
-		'user' => $this->input->post('user',TRUE),
-		'armorst_penyusutan_thn_lalu' => $this->input->post('armorst_penyusutan_thn_lalu',TRUE),
-		'nilai_buku_thn_lalu' => $this->input->post('nilai_buku_thn_lalu',TRUE),
-		'penyusutan_bulan_ini' => $this->input->post('penyusutan_bulan_ini',TRUE),
-		'armorst_penyusutan_bulan_ini' => $this->input->post('armorst_penyusutan_bulan_ini',TRUE),
-		'nilai_buku_bulan_ini' => $this->input->post('nilai_buku_bulan_ini',TRUE),
-	    );
+                'uuid_penyusutan' => $this->input->post('uuid_penyusutan', TRUE),
+                'kelompok_harta' => $this->input->post('kelompok_harta', TRUE),
+                'tanggal_perolehan' => $this->input->post('tanggal_perolehan', TRUE),
+                'harga_perolehan' => $this->input->post('harga_perolehan', TRUE),
+                'user' => $this->input->post('user', TRUE),
+                'armorst_penyusutan_thn_lalu' => $this->input->post('armorst_penyusutan_thn_lalu', TRUE),
+                'nilai_buku_thn_lalu' => $this->input->post('nilai_buku_thn_lalu', TRUE),
+                'penyusutan_bulan_ini' => $this->input->post('penyusutan_bulan_ini', TRUE),
+                'armorst_penyusutan_bulan_ini' => $this->input->post('armorst_penyusutan_bulan_ini', TRUE),
+                'nilai_buku_bulan_ini' => $this->input->post('nilai_buku_bulan_ini', TRUE),
+            );
 
             $this->Tbl_penyusutan_model->update($this->input->post('id', TRUE), $data);
             $this->session->set_flashdata('message', 'Update Record Success');
             redirect(site_url('tbl_penyusutan'));
         }
     }
-    
-    public function delete($id) 
+
+    public function delete($id)
     {
         $row = $this->Tbl_penyusutan_model->get_by_id($id);
 
@@ -174,21 +179,21 @@ class Tbl_penyusutan extends CI_Controller
         }
     }
 
-    public function _rules() 
+    public function _rules()
     {
-	$this->form_validation->set_rules('uuid_penyusutan', 'uuid penyusutan', 'trim|required');
-	$this->form_validation->set_rules('kelompok_harta', 'kelompok harta', 'trim|required');
-	$this->form_validation->set_rules('tanggal_perolehan', 'tanggal perolehan', 'trim|required');
-	$this->form_validation->set_rules('harga_perolehan', 'harga perolehan', 'trim|required|numeric');
-	$this->form_validation->set_rules('user', 'user', 'trim|required');
-	$this->form_validation->set_rules('armorst_penyusutan_thn_lalu', 'armorst penyusutan thn lalu', 'trim|required|numeric');
-	$this->form_validation->set_rules('nilai_buku_thn_lalu', 'nilai buku thn lalu', 'trim|required|numeric');
-	$this->form_validation->set_rules('penyusutan_bulan_ini', 'penyusutan bulan ini', 'trim|required|numeric');
-	$this->form_validation->set_rules('armorst_penyusutan_bulan_ini', 'armorst penyusutan bulan ini', 'trim|required|numeric');
-	$this->form_validation->set_rules('nilai_buku_bulan_ini', 'nilai buku bulan ini', 'trim|required|numeric');
+        $this->form_validation->set_rules('uuid_penyusutan', 'uuid penyusutan', 'trim|required');
+        $this->form_validation->set_rules('kelompok_harta', 'kelompok harta', 'trim|required');
+        $this->form_validation->set_rules('tanggal_perolehan', 'tanggal perolehan', 'trim|required');
+        $this->form_validation->set_rules('harga_perolehan', 'harga perolehan', 'trim|required|numeric');
+        $this->form_validation->set_rules('user', 'user', 'trim|required');
+        $this->form_validation->set_rules('armorst_penyusutan_thn_lalu', 'armorst penyusutan thn lalu', 'trim|required|numeric');
+        $this->form_validation->set_rules('nilai_buku_thn_lalu', 'nilai buku thn lalu', 'trim|required|numeric');
+        $this->form_validation->set_rules('penyusutan_bulan_ini', 'penyusutan bulan ini', 'trim|required|numeric');
+        $this->form_validation->set_rules('armorst_penyusutan_bulan_ini', 'armorst penyusutan bulan ini', 'trim|required|numeric');
+        $this->form_validation->set_rules('nilai_buku_bulan_ini', 'nilai buku bulan ini', 'trim|required|numeric');
 
-	$this->form_validation->set_rules('id', 'id', 'trim');
-	$this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
+        $this->form_validation->set_rules('id', 'id', 'trim');
+        $this->form_validation->set_error_delimiters('<span class="text-danger">', '</span>');
     }
 
     public function excel()
@@ -213,41 +218,40 @@ class Tbl_penyusutan extends CI_Controller
 
         $kolomhead = 0;
         xlsWriteLabel($tablehead, $kolomhead++, "No");
-	xlsWriteLabel($tablehead, $kolomhead++, "Uuid Penyusutan");
-	xlsWriteLabel($tablehead, $kolomhead++, "Kelompok Harta");
-	xlsWriteLabel($tablehead, $kolomhead++, "Tanggal Perolehan");
-	xlsWriteLabel($tablehead, $kolomhead++, "Harga Perolehan");
-	xlsWriteLabel($tablehead, $kolomhead++, "User");
-	xlsWriteLabel($tablehead, $kolomhead++, "Armorst Penyusutan Thn Lalu");
-	xlsWriteLabel($tablehead, $kolomhead++, "Nilai Buku Thn Lalu");
-	xlsWriteLabel($tablehead, $kolomhead++, "Penyusutan Bulan Ini");
-	xlsWriteLabel($tablehead, $kolomhead++, "Armorst Penyusutan Bulan Ini");
-	xlsWriteLabel($tablehead, $kolomhead++, "Nilai Buku Bulan Ini");
+        xlsWriteLabel($tablehead, $kolomhead++, "Uuid Penyusutan");
+        xlsWriteLabel($tablehead, $kolomhead++, "Kelompok Harta");
+        xlsWriteLabel($tablehead, $kolomhead++, "Tanggal Perolehan");
+        xlsWriteLabel($tablehead, $kolomhead++, "Harga Perolehan");
+        xlsWriteLabel($tablehead, $kolomhead++, "User");
+        xlsWriteLabel($tablehead, $kolomhead++, "Armorst Penyusutan Thn Lalu");
+        xlsWriteLabel($tablehead, $kolomhead++, "Nilai Buku Thn Lalu");
+        xlsWriteLabel($tablehead, $kolomhead++, "Penyusutan Bulan Ini");
+        xlsWriteLabel($tablehead, $kolomhead++, "Armorst Penyusutan Bulan Ini");
+        xlsWriteLabel($tablehead, $kolomhead++, "Nilai Buku Bulan Ini");
 
-	foreach ($this->Tbl_penyusutan_model->get_all() as $data) {
+        foreach ($this->Tbl_penyusutan_model->get_all() as $data) {
             $kolombody = 0;
 
             //ubah xlsWriteLabel menjadi xlsWriteNumber untuk kolom numeric
             xlsWriteNumber($tablebody, $kolombody++, $nourut);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->uuid_penyusutan);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->kelompok_harta);
-	    xlsWriteLabel($tablebody, $kolombody++, $data->tanggal_perolehan);
-	    xlsWriteNumber($tablebody, $kolombody++, $data->harga_perolehan);
-	    xlsWriteNumber($tablebody, $kolombody++, $data->user);
-	    xlsWriteNumber($tablebody, $kolombody++, $data->armorst_penyusutan_thn_lalu);
-	    xlsWriteNumber($tablebody, $kolombody++, $data->nilai_buku_thn_lalu);
-	    xlsWriteNumber($tablebody, $kolombody++, $data->penyusutan_bulan_ini);
-	    xlsWriteNumber($tablebody, $kolombody++, $data->armorst_penyusutan_bulan_ini);
-	    xlsWriteNumber($tablebody, $kolombody++, $data->nilai_buku_bulan_ini);
+            xlsWriteLabel($tablebody, $kolombody++, $data->uuid_penyusutan);
+            xlsWriteLabel($tablebody, $kolombody++, $data->kelompok_harta);
+            xlsWriteLabel($tablebody, $kolombody++, $data->tanggal_perolehan);
+            xlsWriteNumber($tablebody, $kolombody++, $data->harga_perolehan);
+            xlsWriteNumber($tablebody, $kolombody++, $data->user);
+            xlsWriteNumber($tablebody, $kolombody++, $data->armorst_penyusutan_thn_lalu);
+            xlsWriteNumber($tablebody, $kolombody++, $data->nilai_buku_thn_lalu);
+            xlsWriteNumber($tablebody, $kolombody++, $data->penyusutan_bulan_ini);
+            xlsWriteNumber($tablebody, $kolombody++, $data->armorst_penyusutan_bulan_ini);
+            xlsWriteNumber($tablebody, $kolombody++, $data->nilai_buku_bulan_ini);
 
-	    $tablebody++;
+            $tablebody++;
             $nourut++;
         }
 
         xlsEOF();
         exit();
     }
-
 }
 
 /* End of file Tbl_penyusutan.php */
