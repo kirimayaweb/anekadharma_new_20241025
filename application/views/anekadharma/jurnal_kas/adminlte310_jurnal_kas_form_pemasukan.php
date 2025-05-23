27ae26dca8e743d178ee153457f90d9b3411dba5
“iwanesia.id” <iwanesia.id@gmail.com>
2025-05-23 12:52:52 +0700

    JURNAL KAS

--- application/views/anekadharma/jurnal_kas/adminlte310_jurnal_kas_form_pemasukan.php
+++ application/views/anekadharma/jurnal_kas/adminlte310_jurnal_kas_form_pemasukan.php
@@ -31,7 +31,7 @@
                         <div class="row">
                             <div class="col-4">
                                 <div class="row">
-                                    <div class="col-12" text-align="center"> <strong>Bebet ( BKM )</strong></div>
+                                    <div class="col-12" text-align="center"> <strong>INPUT KAS</strong></div>
                                 </div>
 
 
