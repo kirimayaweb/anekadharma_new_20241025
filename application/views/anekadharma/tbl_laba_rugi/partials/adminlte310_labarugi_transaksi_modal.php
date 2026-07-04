182840315f7c68df742164f3635674db0a486650
T480 <iwanesia.id@gmail.com>
2026-07-04 21:35:41 +0700

    update laba rugi

--- application/views/anekadharma/tbl_laba_rugi/partials/adminlte310_labarugi_transaksi_modal.php
+++ application/views/anekadharma/tbl_laba_rugi/partials/adminlte310_labarugi_transaksi_modal.php
@@ -4,12 +4,13 @@
         <div class="modal-content labarugi-trx-modal">
             <div class="modal-header">
                 <h5 class="modal-title" id="modalLabarugiTransaksiUnitLabel">
-                    <i class="fa fa-list"></i> Rincian Transaksi — <span id="labarugiTrxKetLabel">-</span> / <span id="labarugiTrxUnitLabel">-</span>
+                    <i class="fa fa-list"></i> Rincian Transaksi — <span id="labarugiTrxKetLabel">-</span><span id="labarugiTrxUnitWrap"> / <span id="labarugiTrxUnitLabel">-</span></span>
                 </h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Tutup"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
                 <div id="labarugiTrxLoading" class="labarugi-trx-loading">Memuat data transaksi...</div>
+                <div id="labarugiTrxInfo" class="alert alert-warning labarugi-trx-info" style="display:none;"></div>
                 <div class="labarugi-trx-total mb-2">Total NS: <strong class="text-danger" id="labarugiTrxTotal">0,00</strong></div>
                 <div class="labarugi-trx-dt-scroll-wrap" id="labarugiTrxTableWrap">
                     <table id="tblLabarugiTransaksiUnit" class="table table-bordered table-hover labarugi-trx-table w-100">
